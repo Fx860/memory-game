1a590d6e1d447e8e7d1e213eb47e80dc4c5692ae
Fx860 <gustafsson860@live.com>
2022-03-05 18:48:04 +0100

    lagt till fler png bilder uppdateat css & js

--- app.js
+++ app.js
@@ -1,100 +1,100 @@
 const cardArray = [
     {
         name: 'Yoda',
-        img: 'images/yoda.png',
+        img: 'images/yoda2.png',
 },
     {
         name: 'Rey',
-        img: 'images/Rey.png',
+        img: 'images/Rey2.png',
 },
     {
         name: 'Kylo Ren',
-        img: 'images/Kylo Ren.png',
+        img: 'images/Kylo Ren2.png',
 },
     {
         name: 'Chewbacca',
-        img: 'images/Chewbacca.png',
+        img: 'images/Chewbacca2.png',
 },
     {
         name: 'Obi-wan Kenobi',
-        img: 'images/Obi-wan Kenobi.png',
+        img: 'images/Obi-wan Kenobi2.png',
 },
     {
         name: 'Leia',
-        img: 'images/Leia.png',
+        img: 'images/Leia2.png',
 },
     {
         name: 'Darth Vader',
-        img: 'images/Darth Vader.png',
+        img: 'images/Darth Vader2.png',
 },
     {
         name: 'Darth Maul',
-        img: 'images/Darth Maul.png',
+        img: 'images/Darth Maul2.png',
 },
     {
         name: 'Boba Fett',
-        img: 'images/Boba Fett.png',
+        img: 'images/Boba Fett2.png',
 },
     {
         name: 'stormtrooper',
-        img: 'images/stormtrooper.png',
+        img: 'images/stormtrooper2.png',
 },
     {
         name: 'General Griveous',
-        img: 'images/General Griveous.png',
+        img: 'images/General Griveous2.png',
 },
     {
         name: 'Ahsoka Tano ',
-        img: 'images/Ahsoka Tano .png',
+        img: 'images/Ahsoka Tano2.png',
 },
     
 {
     name: 'Yoda',
-    img: 'images/yoda.png',
+    img: 'images/yoda2.png',
 },
 {
     name: 'Rey',
-    img: 'images/Rey.png',
+    img: 'images/Rey2.png',
 },
 {
     name: 'Kylo Ren',
-    img: 'images/Kylo Ren.png',
+    img: 'images/Kylo Ren2.png',
 },
 {
     name: 'Chewbacca',
-    img: 'images/Chewbacca.png',
+    img: 'images/Chewbacca2.png',
 },
 {
     name: 'Obi-wan Kenobi',
-    img: 'images/Obi-wan Kenobi.png',
+    img: 'images/Obi-wan Kenobi2.png',
 },
 {
     name: 'Leia',
-    img: 'images/Leia.png',
+    img: 'images/Leia2.png',
 },
 {
     name: 'Darth Vader',
-    img: 'images/Darth Vader.png',
+    img: 'images/Darth Vader2.png',
 },
 {
     name: 'Darth Maul',
-    img: 'images/Darth Maul.png',
+    img: 'images/Darth Maul2.png',
 },
 {
     name: 'Boba Fett',
-    img: 'images/Boba Fett.png',
+    img: 'images/Boba Fett2.png',
 },
 {
     name: 'stormtrooper',
-    img: 'images/stormtrooper.png',
+    img: 'images/stormtrooper2.png',
 },
 {
     name: 'General Griveous',
-    img: 'images/General Griveous.png',
+    img: 'images/General Griveous2.png',
 },
 {
     name: 'Ahsoka Tano ',
-    img: 'images/Ahsoka Tano .png',
+    img: 'images/Ahsoka Tano2.png',
 },
 
 ]
@@ -102,6 +102,7 @@ const cardArray = [
 cardArray.sort( () => 0.5 - Math.random()  )
 
 const gridDisplay = document.querySelector('#grid') 
+const cardChosen = []
   
 
 //create your board
@@ -111,12 +112,26 @@ function crateBoard() {
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
         gridDisplay.appendChild(card)  
-
+        card.addEventListener('click', filpcard )
         
     }
 
 }
 crateBoard()
+console.log(cardArray)
+function filpcard() {
+const cardid = this.getAttribute('data-id')
+console.log (cardArray[cardid].name)
+cardChosen.push(cardArray[cardid].name)  
+
+console.log('clicked', cardid)
+console.log(cardChosen)
+
+this.setAttribute('src', cardArray[cardid].img)
+
+
+
+}
 
 
 
