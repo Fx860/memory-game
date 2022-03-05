ae54f997e42b1de66d22e29944ed2e922b9d43c5
Fx860 <gustafsson860@live.com>
2022-03-05 13:45:17 +0100

    lagt till cardArray i app.js

---
+++ app.js
@@ -0,0 +1,108 @@
+const cardArray = [
+    {
+        name: 'Yoda',
+        img: 'images/yoda.png',
+},
+    {
+        name: 'Rey',
+        img: 'images/Rey.png',
+},
+    {
+        name: 'Kylo Ren',
+        img: 'images/Kylo Ren.png',
+},
+    {
+        name: 'Chewbacca',
+        img: 'images/Chewbacca.png',
+},
+    {
+        name: 'Obi-wan Kenobi',
+        img: 'images/Obi-wan Kenobi.png',
+},
+    {
+        name: 'Leia',
+        img: 'images/Leia.png',
+},
+    {
+        name: 'Darth Vader',
+        img: 'images/Darth Vader.png',
+},
+    {
+        name: 'Darth Maul',
+        img: 'images/Darth Maul.png',
+},
+    {
+        name: 'Boba Fett',
+        img: 'images/Boba Fett.png',
+},
+    {
+        name: 'stormtrooper',
+        img: 'images/stormtrooper.png',
+},
+    {
+        name: 'General Griveous',
+        img: 'images/General Griveous.png',
+},
+    {
+        name: 'Ahsoka Tano ',
+        img: 'images/Ahsoka Tano .png',
+},
+    
+{
+    name: 'Yoda',
+    img: 'images/yoda.png',
+},
+{
+    name: 'Rey',
+    img: 'images/Rey.png',
+},
+{
+    name: 'Kylo Ren',
+    img: 'images/Kylo Ren.png',
+},
+{
+    name: 'Chewbacca',
+    img: 'images/Chewbacca.png',
+},
+{
+    name: 'Obi-wan Kenobi',
+    img: 'images/Obi-wan Kenobi.png',
+},
+{
+    name: 'Leia',
+    img: 'images/Leia.png',
+},
+{
+    name: 'Darth Vader',
+    img: 'images/Darth Vader.png',
+},
+{
+    name: 'Darth Maul',
+    img: 'images/Darth Maul.png',
+},
+{
+    name: 'Boba Fett',
+    img: 'images/Boba Fett.png',
+},
+{
+    name: 'stormtrooper',
+    img: 'images/stormtrooper.png',
+},
+{
+    name: 'General Griveous',
+    img: 'images/General Griveous.png',
+},
+{
+    name: 'Ahsoka Tano ',
+    img: 'images/Ahsoka Tano .png',
+},
+
+]
+
+// console.log(cardArray)
+
+cardArray.sort( () => 0.5 - Math.random()  )
+
+console.log(cardArray)
+  
+
